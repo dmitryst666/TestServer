f2a28e3947b28d7c6e7a03364afebf3cea031fe1
dmitryst666 <dmitryst666@gmail.com>
2018-05-09 16:45:45 +0400

    Some start items added, Nlog added but not used yet

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -1,15 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
+
+//
+// https://habr.com/post/120157/
+//
+
 
 namespace TestServer
 {
+    
+
+
+    class Server
+    {
+        TcpListener Listener; // Объект, принимающий TCP-клиентов
+
+        // Запуск сервера
+        public Server(int Port)
+        {
+            // Создаем "слушателя" для указанного порта
+            Listener = new TcpListener(IPAddress.Any, Port);
+            Listener.Start(); // Запускаем его
+
+            // В бесконечном цикле
+            while (true)
+            {
+
+                // Принимаем новых клиентов и передаем их на обработку новому экземпляру класса Client
+                new Client(Listener.AcceptTcpClient());
+
+            }
+        }
+
+        // Остановка сервера
+        ~Server()
+        {
+            // Если "слушатель" был создан
+            if (Listener != null)
+            {
+                // Остановим его
+                Listener.Stop();
+            }
+        }
+
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            new Server(8080);
         }
     }
+
+
+
+
+    // Класс-обработчик клиента
+    class Client
+    {
+        // Конструктор класса. Ему нужно передавать принятого клиента от TcpListener
+        public Client(TcpClient Client)
+        {
+            // Код простой HTML-странички
+            string Html = "<html><body><h1>It works!</h1></body></html>";
+            // Необходимые заголовки: ответ сервера, тип и длина содержимого. После двух пустых строк - само содержимое
+            string Str = "HTTP/1.1 200 OK\nContent-type: text/html\nContent-Length:" + Html.Length.ToString() + "\n\n" + Html;
+            // Приведем строку к виду массива байт
+            byte[] Buffer = Encoding.ASCII.GetBytes(Str);
+            // Отправим его клиенту
+            Client.GetStream().Write(Buffer, 0, Buffer.Length);
+            // Закроем соединение
+            Client.Close();
+        }
+    }
+
 }
